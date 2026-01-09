f8ea1913d35b15007c220a122de86c674eb3e951
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-09 13:01:51 +0500

    commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
-const api=axios.create({
-  baseURL:'http://localhost:5000/api/admin',
-})
 
-api.interceptors.request.use((config)=>{
-  const token =localStorage.getItem('adminToken');
-  if(token && !config.url.includes('/login')){
-    config.headers.Authorization=`Bearer ${token}`
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + '/api/admin', // use Vite env variable
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('adminToken');
+  if (token && !config.url.includes('/login')) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-})
+});
+
 export default api;
