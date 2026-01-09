a9194f9bb604a819d13b4091c5d9227a6a34641e
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-09 18:13:44 +0500

    commit

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,12 +1,10 @@
+// backend/utils/cloudinary.js
 import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
