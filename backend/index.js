57f87242345fd6bf18211bde379d0f01eae95f21
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-27 15:34:10 +0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://sellora-ifou.vercel.app",
+    "https://sellora-kappa.vercel.app",
     "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
