ac69b205c1e58d1730afdd87eaf9aa427a5bb2af
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-28 12:30:31 +0500

    commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,23 +1,19 @@
 
-// api/api.js
 import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
+  withCredentials: true // important
 });
 
- // Request interceptor to attach auth token
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("authToken"); // jo tum login me save kiya tha
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  const token = localStorage.getItem("authToken");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
-}, (error) => {
-  return Promise.reject(error);
-}); 
+});
 
 export default api;
+
 
 /* https://sellora-drab.vercel.app
 
