3b07c2d46480ffc8ebc34fe97c272a98b6858978
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-28 11:51:39 +0500

    commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,6 +2,6 @@ import { createServer } from "vercel-node-server";
 import app from "../index.js";
 import microCors from "micro-cors";
 
-const cors = microCors({ allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], origin: "*" });
+const cors = microCors({ allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], origin: "https://sellora-4ta9.vercel.app" });
 
 export default createServer(cors(app));
