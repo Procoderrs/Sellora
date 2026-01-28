b1bcb9cac96f6cab5642e31802672ee562f3db03
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-28 14:54:44 +0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,9 @@ app.use(cors({
     "https://sellora-ifou.vercel.app",
     "http://localhost:5173"
   ],
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 /* Routes */
