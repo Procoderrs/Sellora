fb1d802013500dbcc8594861fc0c49042c93342f
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-28 14:51:28 +0500

    commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,5 @@
 import { createServer } from "vercel-node-server";
 import app from "../index.js";
-import microCors from "micro-cors";
 
-const cors = microCors({
-    origin: "https://sellora-4ta9.vercel.app",
-    allowMethods: ["GET","POST","PUT","DELETE","OPTIONS"],
-    allowHeaders: ["Content-Type","Authorization"],
-    allowCredentials: true
-});
-
-export default createServer(cors(app));
+// microCors hatado yahan se, Express wala kafi hai
+export default createServer(app);
