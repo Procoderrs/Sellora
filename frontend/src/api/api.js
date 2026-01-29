5263933d0ab55aa792110cb5966ddcb7490c4474
Zeenat <zeenat.zeni0112@gmail.com>
2026-01-29 13:13:08 +0500

    add

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL +'/api',
-  withCredentials:true,
+  baseURL:`${process.env.VITE_API_URL}/api `,
+  
 });
  
  // Request interceptor to attach auth token
