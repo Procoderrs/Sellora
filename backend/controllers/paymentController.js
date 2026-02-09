01045044e1b9a7d4f43432c4166af5bcd86e9e0f
Zeenat <zeenat.zeni0112@gmail.com>
2026-02-09 18:57:42 +0500

    commit

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -5,34 +5,20 @@ import Order from "../models/orderModel.js";
 
 
 export const createCheckoutSession = async (req, res) => {
-  console.log("FRONTEND_URL AT RUNTIME =", JSON.stringify(process.env.FRONTEND_URL));
-
-  if (
-    !process.env.FRONTEND_URL ||
-    !process.env.FRONTEND_URL.startsWith("https://")
-  ) {
+  const frontendUrl = process.env.FRONTEND_URL;
+  if (!frontendUrl || !frontendUrl.startsWith("https://")) {
     return res.status(500).json({
       message: "Invalid FRONTEND_URL on server",
       value: process.env.FRONTEND_URL,
-      
     });
   }
-
-      console.log(process.env.FRONTEND_URL);
 
   const order = await Order.findById(req.params.orderId);
   if (!order) return res.status(404).json({ message: "Order not found" });
 
-console.log("FRONTEND_URL =", import.meta.FRONTEND_URL);
-console.log("Success URL =", `${import.meta.FRONTEND_URL}/payment-success?orderId=${order._id}`);
-console.log("Cancel URL =", `${import.meta.FRONTEND_URL}/payment-cancel`);
-
-
-
   const session = await stripe.checkout.sessions.create({
     mode: "payment",
     payment_method_types: ["card"],
-
     line_items: order.items.map(item => ({
       price_data: {
         currency: "usd",
@@ -41,13 +27,12 @@ console.log("Cancel URL =", `${import.meta.FRONTEND_URL}/payment-cancel`);
       },
       quantity: item.quantity,
     })),
-
-   success_url: `${frontendUrl}/payment-success?orderId=${order._id}`,
-  cancel_url: `${frontendUrl}/payment-cancel`,
-
+    success_url: `${frontendUrl}/payment-success?orderId=${order._id}`,
+    cancel_url: `${frontendUrl}/payment-cancel`,
     metadata: { orderId: order._id.toString() },
   });
 
   res.json({ url: session.url });
 };
+
 
